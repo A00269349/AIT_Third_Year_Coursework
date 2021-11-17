2701b6531a44e191efd912ffdb7e81b6411e5d54
A00269349 <a00269349@student.ait.ie>
2021-11-17 15:18:35 +0000

    Finished Operating Systems Notes

--- Operating Systems/Lab_1_Thread/src/SimpleThreadTest.java
+++ Operating Systems/Lab_1_Thread/src/SimpleThreadTest.java
@@ -1,13 +1,14 @@
 /**
  * The type Simple thread test.
+ *
  * @author Raphael Salaja
  * @version 10/10/21
  */
-public class SimpleThreadTest
-{
+public class SimpleThreadTest {
     public static void main(String[] args) {
         new SimpleThread("Dog").start();
         new SimpleThread("Cat").start();
         new SimpleThread("Fish").start();
     }
 }
+
